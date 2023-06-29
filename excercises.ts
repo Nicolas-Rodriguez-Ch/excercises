28edd260db4406c149f4d926de38d05e964c8f00
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-06-29 18:30:40 -0500

    does the first step of excercise 2

--- excercises.ts
+++ excercises.ts
@@ -15,6 +15,7 @@
 // > [65] -> [5]
 // > [6, 2, 1] -> [1 , 2]
 // > [60, 6, 5, 4, 3, 2, 7, 7, 29, 1] -> [1, 2, 2, 3, 4, 5, 0]
+const s = 2;
 
 const filter = (n: number[], s: number): void => {
   let restult = n.toLocaleString().split("");
@@ -33,7 +34,6 @@ const filter = (n: number[], s: number): void => {
     count[arr[i]]++;
   }
 
-
   // creates a new array in descending order
   let sortedArr: number[] = [];
   for (let i = count.length - 1; i >= 0; i--) {
@@ -44,4 +44,26 @@ const filter = (n: number[], s: number): void => {
   console.log(sortedArr);
 };
 
-filter([60, 6, 5, 4, 3, 2, 7, 7, 29, 1], 2);
+filter([60, 6, 5, 4, 3, 2, 7, 7, 29, 1], s);
+
+// 2. Code challenge two
+// Write a function that takes in a non-empty array of integers sorted in ascending order and returns a
+// new array of the same length with the squares of the original integers also sorted in ascending
+// order. If the output number is out of the range [0, SS] (for S=6 the range will be [0, 66]), you will
+// delete it of the output array. Please, don’t use built-in sort of your language.
+// Examples with S=6:
+// > {"array": [1, 2, 3, 5, 6, 8, 9]} -> [1, 4, 9, 25, 36, 64]
+// > {"array": [-2, -1]} -> [1, 4]
+// > {"array": [-6, -5, 0, 5, 6]} -> [0, 25, 25, 36, 36]
+// > {"array": [-10, 10]} -> []
+
+const square = (n: number[], s: number): number[] => {
+  const max = s * 11;
+  let newArr: number[] = [];
+  for (let i = 0; i < n.length; i++) {
+    const squareI = n[i] * n[i];
+    squareI < max ? newArr.push(squareI) : "";
+  }
+};
+
+console.log(square([-6, -5, 0, 5, 6], 6));
