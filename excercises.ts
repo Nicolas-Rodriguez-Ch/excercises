29d71bc942475e43c3af6f9c425424c99c29f286
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-06-29 19:29:49 -0500

    starts the third challenge

--- excercises.ts
+++ excercises.ts
@@ -2,19 +2,6 @@
 
 // 1. Code challenge one
 
-// Having a list of n numbers with digits in range [0, S], where n <= 100, switch all list positions in O(n)
-// time.
-// If the input number contains a digit greater or equal than S, you will delete the digit from the
-// number, for example with S=6, 61 becomes 1, and 6 will be deleted from the array. The result
-// should be printed in console/terminal. Please, don’t use built-in sort of your language.
-// Examples with S=6:
-// > [1, 2, 3, 4, 5, 6] -> [5, 4, 3, 2, 1]
-// > [10, 20, 30, 40] -> [40, 30, 20, 10]
-// > [6] -> []
-// > [66] -> []
-// > [65] -> [5]
-// > [6, 2, 1] -> [1 , 2]
-// > [60, 6, 5, 4, 3, 2, 7, 7, 29, 1] -> [1, 2, 2, 3, 4, 5, 0]
 const s = 2;
 
 const filter = (n: number[], s: number): void => {
@@ -28,7 +15,7 @@ const filter = (n: number[], s: number): void => {
     }
   }
 
-  // counts the ocurrences of each number
+  // creates histogram with the number of repetitions in each index
   let count = new Array(s).fill(0);
   for (let i = 0; i < arr.length; i++) {
     count[arr[i]]++;
@@ -47,29 +34,24 @@ const filter = (n: number[], s: number): void => {
 filter([60, 6, 5, 4, 3, 2, 7, 7, 29, 1], s);
 
 // 2. Code challenge two
-// Write a function that takes in a non-empty array of integers sorted in ascending order and returns a
-// new array of the same length with the squares of the original integers also sorted in ascending
-// order. If the output number is out of the range [0, SS] (for S=6 the range will be [0, 66]), you will
-// delete it of the output array. Please, don’t use built-in sort of your language.
-// Examples with S=6:
-// > {"array": [1, 2, 3, 5, 6, 8, 9]} -> [1, 4, 9, 25, 36, 64]
-// > {"array": [-2, -1]} -> [1, 4]
-// > {"array": [-6, -5, 0, 5, 6]} -> [0, 25, 25, 36, 36]
-// > {"array": [-10, 10]} -> []
 
 const square = (n: number[], s: number): number[] => {
   const max = s * 11;
   let newArr: number[] = [];
+
+  // squares the value of the number and then checks if it goes over the max
   for (let i = 0; i < n.length; i++) {
     const squareI = n[i] * n[i];
     squareI <= max ? newArr.push(squareI) : "";
   }
 
+  // creates histogram with the number of repetitions in each index
   const count = new Array(max + 1).fill(0);
   for (let i = 0; i < newArr.length; i++) {
     count[newArr[i]]++;
   }
 
+  // creates a new array with the numbers sorted
   const sortedArr: number[] = [];
   for (let i = 0; i < count.length; i++) {
     for (let j = 0; j < count[i]; j++) {
@@ -80,3 +62,12 @@ const square = (n: number[], s: number): number[] => {
 };
 
 console.log(square([1, 2, 3, 5, 6, 8, 9], s));
+
+// 3. Code challenge three
+
+const change = (coins: number[]): any => {
+  coins = coins.sort((a, b) => a - b);
+  return coins;
+};
+
+console.log(change([5, 7, 1, 1, 2, 3, 22]));
